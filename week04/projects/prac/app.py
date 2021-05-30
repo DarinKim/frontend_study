ee7e61f275eae1a76a3f9ec063c8f86fd6407c3e
Darin Kim <darinkim.dev@gmail.com>
2021-05-30 22:23:00 +0900

    [week04][Flask] GET/POST API

--- week04/projects/prac/app.py
+++ week04/projects/prac/app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+
 ## HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
 ## API 역할을 하는 부분
 @app.route('/test', methods=['GET'])
 def test_get():
@@ -13,5 +16,12 @@ def test_get():
     print(title_receive)
     return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
 
+## POST API
+@app.route('/test', methods=['POST'])
+def test_post():
+    title_receive = request.form['title_give']
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port = 5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
